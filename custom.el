644c073343db809aa1b83885659e4508bf963552
Rhett Sutphin <rhett@detailedbalance.net>
2010-06-22 21:38:27 -0500

    Don't deep-indent method call parameter lists.

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(ruby-deep-arglist nil)
+ '(ruby-deep-indent-paren nil)
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
